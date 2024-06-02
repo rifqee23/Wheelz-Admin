3f4ea01f5aed1db6a7aa1c7be1f065017942ae6b
Anisamitha23 <anisamitha23@gmail.com>
2024-06-02 22:58:28 +0700

    tableDataPelanggan_tes

---
+++ src/layout/tableDataPelanggan.php
@@ -0,0 +1,96 @@
+<table
+                  class="w-full divide-y divide-gray-200 dark:divide-neutral-700"
+                >
+                  <thead>
+                    <tr>
+
+                    <th
+                        scope="col"
+                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                      >
+                       No
+                      </th>
+
+                      <th
+                        scope="col"
+                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                      >
+                        Nama 
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                      >
+                        Alamat
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        class="py-3 text-xs font-medium text-gray-500 uppercase ps-6 text-start dark:text-neutral-500"
+                      >
+                        No Handphone
+                      </th>
+
+                      <th
+                        scope="col"
+                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
+                      >
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    class="divide-y divide-gray-200 dark:divide-neutral-700"
+                  >
+                  
+                    <tr>
+
+                    <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                      1
+                      </td>
+                      <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                      Yanto
+                      </td>
+                      <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                      Kedaton
+                      </td>
+                      <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                      yantokedaton18@gmail.com
+                      </td>
+                      <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                      081216547462
+                      </td>
+                      <td
+                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
+                      >
+                            <div>
+                                <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+                                    Edit
+                                </button>
+                            </div>
+
+                            <div class="mt-4">
+                                <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+                                    Delete
+                                </button>
+                            </div>
+                      </td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
