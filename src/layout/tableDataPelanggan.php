3a563f0a44f7616c99c15ed84155b583eceece50
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-09 00:03:25 +0700

    table

--- src/layout/tableDataPelanggan.php
+++ src/layout/tableDataPelanggan.php
@@ -19,64 +19,60 @@
         No Handphone
       </th>
 
-                      <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        Action
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody
-                    class="divide-y divide-gray-200 dark:divide-neutral-700"
-                  >
-                  
-                    <tr>
-
-                    <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      1
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      Yanto
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      Kedaton
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      yantokedaton18@gmail.com
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      081216547462
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                            <div>
-                                <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
-                                    Edit
-                                </button>
-                            </div>
-
-            <div class="mt-4">
-              <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
-                Delete
-              </button>
-            </div>
-          </td>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        Action
+      </th>
     </tr>
-<?php
-        endwhile;
-      endif;
-?>
+  </thead>
+  <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+
+    <tr>
+
+    <tr>
+
+      <?php
+      include "crud/koneksi.php";
+      $sql = "SELECT u.nama, r.alamat, u.email, u.notelp FROM rincian r JOIN user u ON r.id_user = u.id;";
+      $rs = mysqli_query($conn, $sql);
+      $i = 1;
+      while ($row = mysqli_fetch_assoc($rs)) :
+
+      ?>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php echo $i++; ?>
+        </td>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php
+          echo $row["nama"];
+          ?>
+        </td>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php
+          echo $row["alamat"];
+          ?>
+        </td>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          yantokedaton18@gmail.com
+        </td>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          081216547462
+        </td>
+        <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <div>
+            <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+              Edit
+            </button>
+          </div>
+
+          <div class="mt-4">
+            <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+              Delete
+            </button>
+          </div>
+        </td>
+    </tr>
+  <?php
+      endwhile;
+  ?>
   </tbody>
 </table>
