add14d484db9eeee61749fa40a2e032d0fd6a45f
rifqee23 <rifqifebrianto746@gmail.com>
2024-06-08 20:43:04 +0700

    dir bukti bayar

--- src/crud/upload_gambar.php
+++ src/crud/upload_gambar.php
@@ -2,7 +2,7 @@
 
 include("koneksi.php");
 var_dump($_POST);
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
   $model = $_POST['model'];
   $nopol = $_POST['nopol'];
   $mobil = $_POST['mobil'];
@@ -14,14 +14,12 @@ if(isset($_POST['submit'])) {
   // upload
 
   $folder = "/opt/lampp/htdocs/wheelz\ admin/src/asset/carImg";
-  $folder1 = "/opt/lampp/htdocs/adsi/src/mobil/";
 
   // Mendapatkan nama file asli
   $nama_file_asli = basename($_FILES["gambar"]["name"]);
 
   // Menyusun path lengkap untuk file di folder pertama dan kedua
   $file_in_folder = $folder . $nama_file_asli;
-  $file_in_folder1 = $folder1 . $nama_file_asli;
 
   // Status awal
   $status = 1;
@@ -35,49 +33,49 @@ if(isset($_POST['submit'])) {
 
 
   if ($error === 4) {
-    header("Location: ./../DataMobil.php?alert=belum_upload");
+    echo "<script>alert('gambar belum di upload');</script>";
     $status = 0;
-}
+  }
 
   // Memeriksa apakah file adalah gambar
   $cek = getimagesize($_FILES["gambar"]["tmp_name"]);
   if ($cek !== false) {
-      $status = 1;
+    $status = 1;
   } else {
-    header("Location: ./../DataMobil.php?alert=bukan_gambar");
-      $status = 0;
+    echo "<script>alert('yang di upload bukan gambar');</script>";
+
+    $status = 0;
   }
 
   // Memeriksa apakah file sudah ada
   if (file_exists($file_in_folder) || file_exists($file_in_folder1)) {
     header("Location: ./../DataMobil.php?alert=file_exist");
-      $status = 0;
+    $status = 0;
   }
 
   // Memeriksa ekstensi file
   if ($file_extension !== "jpg" && $file_extension !== "png" && $file_extension !== "jpeg" && $file_extension !== "gif") {
-      echo "Format file tidak didukung";
-      $status = 0;
+    echo "Format file tidak didukung";
+    $status = 0;
   }
 
   // Menyimpan atau menampilkan pesan jika upload berhasil atau gagal
   if ($status == 0) {
-      echo "Upload gambar gagal!";
+    echo "Upload gambar gagal!";
   } else {
-      if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $file_in_folder)) {
-          // Mengunggah file ke folder pertama
-          header("Location: ./../DataMobil.php");
-      
-        //   Menyalin file ke folder kedua
-          if (copy($file_in_folder, $file_in_folder1)) {
-            header("Location: ./../DataMobil.php");
-          } else {
-            header("Location: ./../DataMobil.php?alert=gagal_salin");
-          }
+    if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $file_in_folder)) {
+      // Mengunggah file ke folder pertama
+      header("Location: ./../DataMobil.php");
 
+      //   Menyalin file ke folder kedua
+      if (copy($file_in_folder, $file_in_folder1)) {
+        header("Location: ./../DataMobil.php");
       } else {
-        header("Location: ./../DataMobil.php?alert=failed");
+        header("Location: ./../DataMobil.php?alert=gagal_salin");
       }
+    } else {
+      header("Location: ./../DataMobil.php?alert=failed");
+    }
   }
   // upload end
 
@@ -88,19 +86,17 @@ if(isset($_POST['submit'])) {
 
   // Jika nomor polisi belum ada di database, lakukan penambahan data baru
   if (!mysqli_num_rows($result) > 0) {
-      // Query untuk menambahkan data baru
-      $sql = "INSERT INTO Vehicle (model, nomor_polisi, nama_mobil, seat, car_only, car_only_driver, car_only_driver_fuel, gambar) VALUES ('$model', '$nopol', '$mobil', '$seat', '$only', '$driver', '$fuel', '$nama_file_asli')";
+    // Query untuk menambahkan data baru
+    $sql = "INSERT INTO Vehicle (model, nomor_polisi, nama_mobil, seat, car_only, car_only_driver, car_only_driver_fuel, gambar) VALUES ('$model', '$nopol', '$mobil', '$seat', '$only', '$driver', '$fuel', '$nama_file_asli')";
 
-      // Jalankan query untuk menambahkan data baru
-      if (mysqli_query($conn, $sql)) {
-          echo "New record successfully created.";
-      } else {
-          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-      }
+    // Jalankan query untuk menambahkan data baru
+    if (mysqli_query($conn, $sql)) {
+      echo "New record successfully created.";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
   } else {
-      // Jika nomor polisi sudah ada di database, berikan pesan error
-      echo "Nomor plat sudah ada.";
+    // Jika nomor polisi sudah ada di database, berikan pesan error
+    echo "Nomor plat sudah ada.";
   }
 }
-
-?>
