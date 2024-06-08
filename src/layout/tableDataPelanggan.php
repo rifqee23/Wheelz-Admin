ffa78d9bf7c28f788bedd151406ccec36ef39e57
Anisamitha23 <anisamitha23@gmail.com>
2024-06-08 23:56:51 +0700

    koneksi data pelanggan

--- src/layout/tableDataPelanggan.php
+++ src/layout/tableDataPelanggan.php
@@ -1,96 +1,84 @@
-<table
-                  class="w-full divide-y divide-gray-200 dark:divide-neutral-700"
-                >
-                  <thead>
-                    <tr>
+<table class="w-full divide-y divide-gray-200 dark:divide-neutral-700">
+  <thead>
+    <tr>
 
-                    <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                       No
-                      </th>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        No
+      </th>
 
-                      <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        Nama 
-                      </th>
-                      <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        Alamat
-                      </th>
-                      <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        Email
-                      </th>
-                      <th
-                        scope="col"
-                        class="py-3 text-xs font-medium text-gray-500 uppercase ps-6 text-start dark:text-neutral-500"
-                      >
-                        No Handphone
-                      </th>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        Nama
+      </th>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        Alamat
+      </th>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        Email
+      </th>
+      <th scope="col" class="py-3 text-xs font-medium text-gray-500 uppercase ps-6 text-start dark:text-neutral-500">
+        No Handphone
+      </th>
 
-                      <th
-                        scope="col"
-                        class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500"
-                      >
-                        Action
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody
-                    class="divide-y divide-gray-200 dark:divide-neutral-700"
-                  >
-                  
-                    <tr>
+      <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase text-start dark:text-neutral-500">
+        Action
+      </th>
+    </tr>
+  </thead>
+  <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
 
-                    <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      1
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      Yanto
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      Kedaton
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      yantokedaton18@gmail.com
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                      081216547462
-                      </td>
-                      <td
-                        class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200"
-                      >
-                            <div>
-                                <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
-                                    Edit
-                                </button>
-                            </div>
+    <tr>
 
-                            <div class="mt-4">
-                                <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
-                                    Delete
-                                </button>
-                            </div>
-                      </td>
-                    </tr>
-                    
-                  </tbody>
-                </table>
+      <?php
+      include "crud/koneksi.php";
+      $sql = "SELECT u.nama, r.alamat, u.email, u.notelp FROM rincian r JOIN user u ON r.id_user = u.id;";
+      $rs = mysqli_query($conn, $sql);
+      if (mysqli_num_rows($rs) > 0) : 
+        $i=1;
+        while ($row = mysqli_fetch_assoc($rs)) :
+
+      ?>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php
+            echo $i++;
+            ?>
+          </td>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+            <?php
+            echo $row["nama"];
+            ?>
+          </td>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+            <?php
+            echo $row["alamat"];
+            ?>
+          </td>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php
+            echo $row["email"];
+            ?>
+          </td>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+          <?php
+            echo $row["notelp"];
+            ?>
+          </td>
+          <td class="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap dark:text-neutral-200">
+            <div>
+              <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+                Edit
+              </button>
+            </div>
+
+            <div class="mt-4">
+              <button class="text-sm font-semibold text-blue-600 hover:text-blue-800">
+                Delete
+              </button>
+            </div>
+          </td>
+    </tr>
+<?php
+        endwhile;
+      endif;
+?>
+  </tbody>
+</table>
